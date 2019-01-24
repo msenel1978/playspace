b27b494adfc9fad522ed3c8d070a36d4d45f83ab
Murat Senel <msenel@Murats-MacBook-Pro.local>
2019-01-24 05:51:16 -0500

    max_population: Formatting edits

--- max_population.c
+++ max_population.c
@@ -22,17 +22,18 @@ int death_yt[10] = {2019, 2020, 1976, 1914, 2011, 1938, 2014, 2008, 2100, 2060};
 */
 
 person_t people[MAX_POPULATION] = { {2000, 2019},
-						{1998, 2020},
-						{1900, 1976},
-						{1876, 1914},
-						{1976, 2011},
-						{1923, 1938},
-						{1978, 2014},
-						{1996, 2008},
-						{2014, 2100},
-						{1976, 2060} };
+				{1998, 2020},
+				{1900, 1976},
+				{1876, 1914},
+				{1976, 2011},
+				{1923, 1938},
+				{1978, 2014},
+				{1996, 2008},
+				{2014, 2100},
+				{1976, 2060} };
 
-static int init_arry(int delta_arry[], int len){
+static int
+init_arry(int delta_arry[], int len){
 	int i = 0;
 
 	for (i = 0; i < len; i++)
@@ -41,7 +42,8 @@ static int init_arry(int delta_arry[], int len){
 	return 1;
 }
 
-static int cumulative_arry(int delta_arry[], int len){
+static int
+cumulative_arry(int delta_arry[], int len){
 	int i;
 
 	for (i = 1; i < len; i++)
@@ -51,7 +53,8 @@ static int cumulative_arry(int delta_arry[], int len){
 
 }
 
-static int print_arry_content(int arry[], int len) {
+static int
+print_arry_content(int arry[], int len) {
 	int i = 0;
 
 	printf("\n Array: ");
@@ -62,7 +65,8 @@ static int print_arry_content(int arry[], int len) {
 	return 1;
 }
 
-static int find_max_birth_yr(person_t population[]) {
+static int
+find_max_birth_yr(person_t population[]) {
 	int max_yr = population[0].birth_yr;
 	int i;
 
@@ -74,7 +78,8 @@ static int find_max_birth_yr(person_t population[]) {
 	return max_yr;
 }
 
-static int find_min_birth_yr(person_t population[]) {
+static int
+find_min_birth_yr(person_t population[]) {
 	int min_yr = population[0].birth_yr;
 	int i;
 
@@ -86,7 +91,8 @@ static int find_min_birth_yr(person_t population[]) {
 	return min_yr;
 }
 
-static int find_max_yr(int cum_arry[], int init_yr, int len) {
+static int
+find_max_yr(int cum_arry[], int init_yr, int len) {
 	int i = 0, max_yr = 0;
 
 	for (i = 0; i < len; i++) {
@@ -97,7 +103,8 @@ static int find_max_yr(int cum_arry[], int init_yr, int len) {
 	return (max_yr + init_yr);
 }
 
-static int find_max_pop_yr(person_t population[]) {
+static int
+find_max_pop_yr(person_t population[]) {
 	int max_birth_yr = find_max_birth_yr(population);
 	int min_birth_yr = find_min_birth_yr(population);
 	int time_span = max_birth_yr - min_birth_yr + 1;
